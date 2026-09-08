4494448814d58958ad859ee1b98cbf71de3d0723
Enzo Morgan <112134107+enzomorgan@users.noreply.github.com>
2026-09-07 21:47:17 -0300

    Delete ARENA_MANAGER/backend directory

--- ARENA_MANAGER/backend/apps/matches/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class MatchesConfig(AppConfig):
-    defualt_auto_field = "django.db.models.BigAutoField"
-    name = "apps.matches"
-    
-    def ready(self):
-        import apps.matches.signals
--- ARENA_MANAGER/backend/apps/teams/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Team
-
-@admin.register(Team)
-class TeamAdmin(admin.ModelAdmin):
-    list_display = ("name", "city", "manager", "founded_year")
-    search_fields = ("name", "city")
-    list_filter = ("city",)
